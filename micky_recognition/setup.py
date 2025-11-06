e2891c72f254b6f22653bb1308ad23cecc7ddf8f
Gabriel Torres <gabrieltlt721@gmail.com>
2025-11-06 10:52:35 -0300

    :construction: updated setup.py and requirements.txt

--- micky_recognition/setup.py
+++ micky_recognition/setup.py
@@ -22,7 +22,6 @@ setup(
     maintainer_email='gabrieltlt721@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             # 'reference_feature = micky_recognition.reference_feature:main',
